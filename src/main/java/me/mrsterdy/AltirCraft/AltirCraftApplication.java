067c54b06a833afae38f8c795b1fd698976b2f65
MrSterdy <mrsterdy@gmail.com>
2022-06-23 22:06:17 +0400

    Initial commit

---
+++ src/main/java/me/mrsterdy/AltirCraft/AltirCraftApplication.java
@@ -0,0 +1,13 @@
+package me.mrsterdy.AltirCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltirCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltirCraftApplication.class, args);
+	}
+
+}
